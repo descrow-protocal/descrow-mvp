d0cf313b2531ace6fbd587143f89d4ceac133db2
oumaoumag <ouma.godwin10@gmail.com>
2025-11-16 10:16:38 +0300

    config: add hardhat config

---
+++ web3/hardhat.config.js
@@ -0,0 +1,13 @@
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+
+module.exports = {
+  solidity: '0.8.20',
+  networks: {
+    moonbaseAlpha: {
+      url: 'https://rpc.api.moonbase.moonbeam.network',
+      chainId: 1287,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
+};
